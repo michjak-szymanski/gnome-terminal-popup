7f0abef023fa6624a59f692d6c65247b8c3135f0
Michał Szymański <michjak.szymanski@gmail.com>
2021-06-27 17:17:54 +0200

    added toggle feature (hide if all visible)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # gnome-terminal-popup
-Bash script to show Gnome terminal window(s) on current desktop. 
+Bash script to toggle show/hide Gnome terminal window(s) on current desktop. 
 This could be used as a native alternative to drop down terminals on Gnome desktop environment.
 
 # How to use it
@@ -7,5 +7,6 @@ This script can be bound to keyboard shortcut. In such way the Gnome terminal wi
 
 Obviously it can be also exeucted manually.
 
-# Planned additional features
-- **toggle visibility:** if Gnome terminal is already visible then executing this script should hide it and another way around if is not.
+# Dependencies
+- **xdotool**, can be installed with `sudo apt install xdotool`
+- **wmctrl**, can be installed with `sudo apt install wmctrl`
--- gnome_terminal_popup.sh
+++ gnome_terminal_popup.sh
@@ -2,6 +2,8 @@
 #
 # This script should pop out Gnome terminal window(s).
 # If none exists yet then a new one will be created.
+# On the other hand, if all terminal's windows are already visible,
+# then all of them will be minimized (toggle feature).
 # 
 # Author: Michał Szymański <michjak.szymanski@gmail.com>
 #
@@ -21,6 +23,33 @@ if [ -z "${gnome_server_pid//}" ]
 
 # get (all) open gnome-terminal windows ids
 gnome_server_window_ids=$(wmctrl -lp | grep $gnome_server_pid | awk -F ' ' '{print $1}')
+
+# hide all gnome terminal windows if they are open
+total_gnome_server_windows=0
+visible_gnome_server_windows=0
+
+for window_id in $gnome_server_window_ids 
+    do 
+        window_info=$(xprop -id $window_id)
+        window_state_normal_matches=$(echo $window_info | grep 'window state: Normal' -c)
+
+        if [[ $window_state_normal_matches -eq "1" ]]
+            then
+                visible_gnome_server_windows=$(($visible_gnome_server_windows + 1))
+            fi 
+        
+        total_gnome_server_windows=$(($total_gnome_server_windows + 1))
+    done
+
+if [ $total_gnome_server_windows -eq $visible_gnome_server_windows ]
+    then 
+        for window_id in $gnome_server_window_ids 
+            do 
+                xdotool windowminimize $window_id
+            done
+
+        exit 0
+    fi
 
 # show all gnome-terminal windows on current desktop
 for window_id in $gnome_server_window_ids 
